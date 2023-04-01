3811487dec09e203f1f846319e8224a9c5c03d37
Haley Akin <wasatchtimpanogos@outlook.com>
2023-03-31 19:42:59 -0600

    save

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 
 
 
+
 export const Home = () => {
+  
   return (
     <div id='home' >
        <div className='homeScreen'>
-          <h1 className='hscreen'>home screen</h1>
+          <div className='flexHome'>
+            <img src='../assets/home.PNG' alt='outdoor movie image' className='homeImg' />
+            <div className='home'>
+              <h1 className='homeh1'>Welcome to Site Name!</h1>
+              <p className='homep'>description of site, description of site, description of site...</p>
+            </div>
+          </div>
         </div> 
     </div>
   )
